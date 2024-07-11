7b1b148912aa2a8519880d3cf19f21e4432d4bb1
Your Name <alessanrospedroso@outlook.com>
2024-07-11 09:20:30 -0300

    primeira parte do curso finalizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const pessoas = require('./pessoasRoute.js');
+const categoria = require('./categoriasRoute.js');
+const cursos = require('./cursosRoute.js');
 
 module.exports = (app) => {
-	app.use(express.json(), pessoas);
+	app.use(express.json(), pessoas, categoria, cursos);
 };
