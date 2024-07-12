30000d31102af2238f15947d73d52a9913badb3d
Your Name <alessanrospedroso@outlook.com>
2024-07-12 08:15:04 -0300

    criando softs delete

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -7,8 +7,8 @@ class Controller {
 		try {
 			const listaDeRegistros = await this.entidadeService.pegaTodosOsRegistros();
 			return res.status(200).json(listaDeRegistros);
-		} catch (error) {
-			//erro
+		} catch (erro) {
+			return res.status(500).json({ erro: erro.message });
 		}
 	}
 
@@ -18,7 +18,7 @@ class Controller {
 			const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
 			return res.status(200).json(umRegistro);
 		} catch (erro) {
-			// erro
+			return res.status(500).json({ erro: erro.message });
 		}
 	}
 
@@ -28,7 +28,7 @@ class Controller {
 			const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
 			return res.status(200).json(novoRegistroCriado);
 		} catch (erro) {
-			// erro
+			return res.status(500).json({ erro: erro.message });
 		}
 	}
 
@@ -42,7 +42,9 @@ class Controller {
 				return res.status(400).json({ mensagem: 'registro não foi atualizado' });
 			}
 			return res.status(200).json({ mensagem: 'Atualizado com sucesso' });
-		} catch (error) {}
+		} catch (erro) {
+			return res.status(500).json({ erro: erro.message });
+		}
 	}
 
 	async exclui(req, res) {
@@ -50,8 +52,8 @@ class Controller {
 		try {
 			await this.entidadeService.excluiRegistro(Number(id));
 			return res.status(200).json({ mensagem: `id ${id} deletado` });
-		} catch (error) {
-			return res.status(500).json(error.message);
+		} catch (erro) {
+			return res.status(500).json({ erro: erro.message });
 		}
 	}
 }
