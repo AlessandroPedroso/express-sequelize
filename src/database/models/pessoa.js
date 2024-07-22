ce21f65962daa6ffb42b02b06114876618724b99
Your Name <alessanrospedroso@outlook.com>
2024-07-22 08:17:04 -0300

    Validação customizada

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,6 @@
 const { Model } = require('sequelize');
+const isCpfValido = require('../../utils/validaCpfHelper.js');
+
 module.exports = (sequelize, DataTypes) => {
 	// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 	class Pessoa extends Model {
@@ -17,7 +19,15 @@ module.exports = (sequelize, DataTypes) => {
 	}
 	Pessoa.init(
 		{
-			nome: DataTypes.STRING,
+			nome: {
+				type: DataTypes.STRING,
+				validate: {
+					len: {
+						args: [3, 30],
+						msg: 'o campo nome deve ter no mínimo 3 caracteres',
+					},
+				},
+			},
 			email: {
 				type: DataTypes.STRING,
 				validate: {
@@ -27,7 +37,14 @@ module.exports = (sequelize, DataTypes) => {
 					},
 				},
 			},
-			cpf: DataTypes.STRING,
+			cpf: {
+				type: DataTypes.STRING,
+				validate: {
+					cpfEhValido: (cpf) => {
+						if (!isCpfValido(cpf)) throw new Error('numero de CPF inválido');
+					},
+				},
+			},
 			ativo: DataTypes.BOOLEAN,
 			role: DataTypes.STRING,
 		},
